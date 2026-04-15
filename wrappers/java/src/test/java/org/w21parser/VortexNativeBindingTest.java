9d223a41ae18bdc436feb51bdcc46f2e616c6c78
Fábio Pereira da Silva <fabioegel@gmail.com>
2026-04-15 10:35:54 -0300

    Finishing validateErrorCodesContract test

--- wrappers/java/src/test/java/org/w21parser/VortexNativeBindingTest.java
+++ wrappers/java/src/test/java/org/w21parser/VortexNativeBindingTest.java
@@ -39,8 +39,41 @@ public class VortexNativeBindingTest {
         // Tests if JNI_W21_BSON_OBJECT_ALREADY_PARSED = 4090 for native BSON alloc'd and
         // JNI_W21_JSON_STRING_ALREADY_PARSED = 4091 JSON string alloc'd is returned from C code
         // when parsed object is already allocated
+        //
+        // On loading document and CPU/Memory statistics:
+        // Test if JNI_W21_RESOURCE_STAT_NOT_FINISHED_CORRECTLY_OR_NOT_PARSED = 4074 for BSON not parsed or finished
+        // Test if JNI_W21_RESOURCE_STAT_JSON_NOT_FINISHED_CORRECTLY_OR_NOT_PARSED = 4075 for JSON not parsed or finished
         parser1.readFromFile("TestFiles/xmls/strict_valid/BhaRun.xml");
 
+        Method jniMethodStat1 = W21ParserLoader.class.getDeclaredMethod("jniStatParseTotalCycles");
+        jniMethodStat1.setAccessible(true);
+
+        try {
+            jniMethodStat1.invoke(parser1);
+            fail("jniStatParseTotalCycles method should NOT load CPU cycles before BSON parsing");
+        } catch (InvocationTargetException e) {
+            Throwable cause = e.getCause();
+            if (cause instanceof W21Exception) {
+                W21Exception ex = (W21Exception) cause;
+                assertEquals("JNI contract violated: Error code JNI_W21_RESOURCE_STAT_NOT_FINISHED_CORRECTLY_OR_NOT_PARSED changed in C code",
+                        4074, ex.error);
+            }
+        }
+
+        Method jniMethodStat2 = W21ParserLoader.class.getDeclaredMethod("jniStatParseJsonTotalCycles");
+        jniMethodStat2.setAccessible(true);
+
+        try {
+            jniMethodStat2.invoke(parser1);
+            fail("jniStatParseJsonTotalCycles method should NOT load CPU cycles before JSON string parsing");
+        } catch (InvocationTargetException e) {
+            Throwable cause = e.getCause();
+            if (cause instanceof W21Exception) {
+                W21Exception ex = (W21Exception) cause;
+                assertEquals("JNI contract violated: Error code JNI_W21_RESOURCE_STAT_JSON_NOT_FINISHED_CORRECTLY_OR_NOT_PARSED changed in C code",
+                        4075, ex.error);
+            }
+        }
         // ALLOC'D BSON OBJECT
         // Access private native method
         Method jniMethod = W21ParserLoader.class.getDeclaredMethod("jniParse");
@@ -48,6 +81,8 @@ public class VortexNativeBindingTest {
 
         // First success execution: invoke C structs to BSON object transformation
         jniMethod.invoke(parser1);
+
+        assertTrue("CPU cycles on BSON parsing should be greater than 0", (Long)jniMethodStat1.invoke(parser1) > 0);
 
         try {
             // Second execution: Should throw exception because
@@ -71,6 +106,8 @@ public class VortexNativeBindingTest {
 
         // First success execution: invoke C structs to UTF-8 JSON string transformation
         jniMethod.invoke(parser1);
+
+        assertTrue("CPU cycles on JSON string parsing should be greater than 0", (Long)jniMethodStat2.invoke(parser1) > 0);
 
         try {
             // Second execution: Should throw exception because
