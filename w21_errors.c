65c2e7ff95c3cdb69048456e5444ceea077d2eb3
Fábio Pereira da Silva <fabioegel@gmail.com>
2026-07-09 20:45:49 -0300

    Implement detailed error message in C core

--- w21_errors.c
+++ w21_errors.c
@@ -14,6 +14,16 @@ static W21_ERROR_DETAIL w21_error_list[] =
     SET_W21_ERROR_DETAIL(E_W21_CONFIG_ALREADY_ALLOCD, "Pointer has alread alloc'd object")
     SET_W21_ERROR_DETAIL(E_W21_REQUIRE_PATH_AND_FILENAME, "Input Witsml 2.1: Is require path and file name")
     SET_W21_ERROR_DETAIL(E_W21_ERROR_OPENING_FILE, "Input WITSML 2.1 document: Could not open document")
+    SET_W21_ERROR_DETAIL(E_W21_ERROR_ALLOC_W21_OBJECT_STRUCT, "Unable to alloc memory to WITSML 2.1 object")
+    SET_W21_ERROR_DETAIL(E_21_ERROR_IN_OBJECT_ALREADY_ALLOC, "WITSML 2.1 object already alloc'd")
+    SET_W21_ERROR_DETAIL(E_W21_ERROR_CREATE_IN_BSON_OBJECT, "Input mode: Error on allocate BSON object. Unable to parse WITSML 2.1 object")
+    SET_W21_ERROR_DETAIL(E_W21_ERROR_OBJECT_NULL_OR_PARSER_ERROR, "Input mode: WITSML 2.1 object is NULL or parsing error")
+    SET_W21_ERROR_DETAIL(E_W21_ERROR_SET_UTF8_STRING_IN_BSON, "Unable to set UTF-8 string in BSON")
+    SET_W21_ERROR_DETAIL(E_W21_ERROR_BEGIN_BSON_ROOT_OBJECT, "Input mode: Unable to begin BSON root object")
+    SET_W21_ERROR_DETAIL(E_W21_ERROR_END_BSON_ROOT_OBJECT, "Input mode: Unable to end BSON root object")
+    SET_W21_ERROR_DETAIL(E_W21_ERROR_COULD_NOT_BUILD_UTF8_STRING_ARRAY, "Input mode: Unable to build UTF-8 string array")
+    SET_W21_ERROR_DETAIL(E_W21_ERROR_COULD_NOT_ADD_UTF8_STRING_IN_ARRAY, "Input mode: Could not add UTF-8 string into an array")
+    SET_W21_ERROR_DETAIL(E_W21_ERROR_UTF8_STRING_IN_ARRAY_NULL, "Input mode: NULL UTF-8. Could not add in array")
     // TODO Add error tag and description to allow human understand what 
     // is happening without undestand parser structure
     {0, NULL, NULL}
